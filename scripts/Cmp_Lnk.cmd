8870205b5c442c55a4a3c8edc38e0caf0db200bb
General Jack O'Neill <68293825+StarGate-One@users.noreply.github.com>
2023-02-04 22:37:14 -0500

    Fix lib linkage

--- scripts/Cmp_Lnk.cmd
+++ scripts/Cmp_Lnk.cmd
@@ -1,3 +1,3 @@
 nasm -f win64 -o %1.obj %1.asm -l %1.lst -Lb -Ld -Le -Lf -Lm -Lp -Ls -Lw -Ov
 
-link %1.obj /subsystem:console /entry:main /out:%1.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib shell32.lib
+link %1.obj /subsystem:console /entry:main /out:%1.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib shell32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib
